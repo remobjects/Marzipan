c0646b736cee26d13197cc3744cfa56dd06b0d97
Carlo Kok <ck@remobjects.com>
2013-05-26 20:05:24 +0200

    0:more work.

--- Importer/Importer.pas
+++ Importer/Importer.pas
@@ -4,15 +4,21 @@ interface
 
 uses
   System.Collections.Generic,
-  System.Text;
+  System.Linq,
+  System.Text,
+  Mono.Cecil;
 
 type
   Importer = public class
   private
     fSettings: ImporterSettings;
+    fLibraries: List<ModuleDefinition> := new List<ModuleDefinition>;
+    fTypes: List<TypeDefinition> := new List<TypeDefinition>;
+    fImportNameMapping: Dictionary<String, String> := new Dictionary<String,String>;
   protected
   public
     constructor(aSettings: ImporterSettings);
+    event Log: Action<String> raise;
 
     method Run;
   end;
@@ -22,10 +28,38 @@ implementation
 constructor Importer(aSettings: ImporterSettings);
 begin
   fSettings := aSettings;
+  fImportNameMapping.Add('System.Object', 'MZObject');
+  fImportNameMapping.Add('System.String', 'MZString');
+  fImportNameMapping.Add('System.SByte', 'int8_t');
+  fImportNameMapping.Add('System.Byte', 'uint8_t');
+  fImportNameMapping.Add('System.Int16', 'int16_t');
+  fImportNameMapping.Add('System.UInt16', 'uint16_t');
+  fImportNameMapping.Add('System.Int32', 'int32_t');
+  fImportNameMapping.Add('System.UInt32', 'uint32_t');
+  fImportNameMapping.Add('System.Int64', 'int64_t');
+  fImportNameMapping.Add('System.UInt64', 'uint64_t');
+  fImportNameMapping.Add('System.IntPtr', 'intptr_t');
+  fImportNameMapping.Add('System.UIntPtr', 'uintptr_t');
 end;
 
 method Importer.Run;
 begin
+  Log('Loading libraries');
+  for each el in fSettings.Libraries do begin
+    Log('  Loading '+el);
+    fLibraries.Add(ModuleDefinition.ReadModule(el));
+  end;
+  Log('Resolving types');
+  for each el in fSettings.Types do begin
+    var lLib := fLibraries.SelectMany(a -> a.Types.Where(b -> b.FullName = el.Name)).ToArray;
+    if lLib.Count = 0 then
+      raise new Exception('Type "'+el.Name+'" was not found')
+    else
+      fTypes.Add(lLib[0]);
+    var lNewName := fSettings.Prefix+ lLib[0].Name;
+    Log('Adding type '+lNewName+' from '+lLib[0].FullName);
+    fImportNameMapping.Add(lLib[0].FullName, lNewName);
+  end;
 
 end;
 
--- Importer/ImporterSettings.pas
+++ Importer/ImporterSettings.pas
@@ -20,6 +20,8 @@ type
     property Types: List<ImportType> read fTypes;
     property &Namespace: String;
     property OutputType: OutputType;
+    property OutputFilename: String;
+    property Prefix: String;
 
     method LoadFromXml(aFN: String);
     method LoadFromXml(aDoc: XDocument);
@@ -44,6 +46,13 @@ begin
   &Namespace := aDoc.Root.Element('namespace'):Value;
   if aDoc.Root.Element('outputtype'):Value:ToLowerInvariant = 'objc' then
     OutputType := OutputType.ObjC;
+  OutputFilename := aDoc.Root.Element('outputfilename'):Value;
+  Prefix := aDoc.Root.Element('prefix'):Value;
+  case OutputType of
+    OutputType.ObjC: if OutputFilename = nil then OutputFilename := 'default.h';
+  else
+    if OutputFilename = nil then OutputFilename := 'default.pas';
+  end;
   var lLibs := aDoc.Root.Elements('libraries');
   if lLibs = nil then raise new Exception('"Libraries" node missing');
   Libraries.AddRange(lLibs.Elements('library').Select(a->a.Value));
